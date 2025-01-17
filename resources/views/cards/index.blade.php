f452bf2a5991015ab532cde7f1b6310f2750699e
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-01-17 21:10:06 +0100

    Crear componente de barra navegadora

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -1,3 +1,10 @@
+@extends('layouts.app') <!-- Extiende la plantilla principal -->
+
+@section('title', 'Inicio') <!-- Título de la página -->
+
+@section('content')
+<x-navbar />
 <h1>Lista de Cartas</h1>
 
 <x-card-list :cards="$cards" />
+@endsection
