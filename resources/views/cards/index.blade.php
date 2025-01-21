a9cb81d3e6b30baeeba3a316df3ae7406187126e
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-01-21 19:12:09 +0100

    Ver cartas

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -1,14 +1,39 @@
-@extends('layouts.navbar-layout') <!-- Extiende la plantilla principal -->
+@extends('layouts.navbar-layout')
 
-@section('title', 'Cards') <!-- Título de la página -->
+@section('title', 'Cards')
 
 @section('additional-css')
-    <link rel="stylesheet" href="{{ asset('css/cards.css') }}">
+    <link rel="stylesheet" href="{{ asset('View/css/cards.css') }}">
 @endsection
 
 @section('content')
+    <div class="row">
+        @forelse ($cards->items() as $card) <!-- Usamos forelse para manejar el caso de no haber cartas -->
+        <div class="col-md-4">
+            <div class="card">
+                <img src="{{ $card->images['small'] }}" alt="Imagen de {{ $card->name }}">
 
-    <h1>Lista de Cartas</h1>
 
-    <x-card-list :cards="$cards"/>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $card->name }}</h5>
+                    <p class="card-text">Rareza:
+                        @if ($card->rarity)
+                            {{ $card->rarity->name }}
+                        @else
+                            <em>No tiene rareza</em>
+                        @endif
+                    </p>
+                </div>
+            </div>
+        </div>
+        @empty
+            <p>{{ $message }}</p> <!-- Mostrar mensaje si no hay cartas -->
+        @endforelse
+    </div>
+
+    <div class="pagination-container">
+        {{ $cards->links('vendor.pagination.tailwind') }}
+    </div>
+
+
 @endsection
--- resources/views/components/card-list.blade.php
+++ resources/views/components/card-list.blade.php
@@ -1,23 +1,32 @@
 <div class="row">
-    @if($cards->isEmpty())
-        <p>No hay cartas disponibles.</p>
-    @else
-        @foreach ($cards as $card)
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="{{ $card->images['small'] }}" alt="Imagen de {{ $card->name }}" style="width: 100px; height: 100px;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $card->name }}</h5>
-                        <p class="card-text">Rareza:
-                            @if ($card->rarity)
-                                {{ $card->rarity->name }}
-                            @else
-                                <em>No tiene rareza</em>
-                            @endif
-                        </p>
-                    </div>
-                </div>
+    @forelse ($cards->items() as $card) <!-- Usamos forelse para manejar el caso de no haber cartas -->
+    <div class="col-md-4">
+        <div class="card">
+            <!-- Comprobamos si 'images' es un array y si la clave 'small' está presente -->
+            @if (isset($card->images['small']) && !empty($card->images['small']))
+                <img src="{{ $card->images['small'] }}" alt="Imagen de {{ $card->name }}" style="width: 100px; height: 100px;">
+            @else
+                <p>No imagen disponible</p>
+            @endif
+
+            <div class="card-body">
+                <h5 class="card-title">{{ $card->name }}</h5>
+                <p class="card-text">Rareza:
+                    @if ($card->rarity)
+                        {{ $card->rarity->name }}
+                    @else
+                        <em>No tiene rareza</em>
+                    @endif
+                </p>
             </div>
-        @endforeach
-    @endif
+        </div>
+    </div>
+    @empty
+        <p>{{ $message }}</p> <!-- Mostrar mensaje si no hay cartas -->
+    @endforelse
+</div>
+
+<!-- Paginación -->
+<div class="pagination-container">
+    {{ $cards->links() }}
 </div>
