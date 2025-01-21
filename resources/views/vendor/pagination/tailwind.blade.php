c888c2f84783924ee139a87dce1ff2fb501d5dd1
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-01-21 19:11:42 +0100

    Paginacion provisional

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,43 @@
+@if ($paginator->hasPages())
+    <nav role="navigation" aria-label="Pagination" class="pagination">
+        {{-- Botón de "Previous" --}}
+        <span class="pagination-prev">
+            @if ($paginator->onFirstPage())
+                <span class="disabled">« Previous</span>
+            @else
+                <a href="{{ $paginator->previousPageUrl() }}" rel="prev">« Previous</a>
+            @endif
+        </span>
+
+        {{-- Mostrar los links de las páginas --}}
+        <span class="pagination-links">
+            @foreach ($elements as $element)
+                @if (is_string($element))
+                    <span class="disabled">{{ $element }}</span>
+                @elseif (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <span class="current">{{ $page }}</span>
+                        @else
+                            <a href="{{ $url }}">{{ $page }}</a>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+        </span>
+
+        {{-- Botón de "Next" --}}
+        <span class="pagination-next">
+            @if ($paginator->hasMorePages())
+                <a href="{{ $paginator->nextPageUrl() }}" rel="next">Next »</a>
+            @else
+                <span class="disabled">Next »</span>
+            @endif
+        </span>
+    </nav>
+
+    {{-- Mostrar el rango actual de elementos --}}
+    <div class="pagination-info">
+        Showing {{ $paginator->firstItem() }} to {{ $paginator->lastItem() }} of {{ $paginator->total() }} cards
+    </div>
+@endif
