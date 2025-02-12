91d3848d938784a9ced28f1c6dbb5e65caf7401b
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-02-12 13:18:43 +0100

    Enviar correo de verificación

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,50 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@extends('layouts.navbar-layout')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+@section('title', 'Email Verification')
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+@section('additional-css')
+    <link rel="stylesheet" href="{{ asset('View/css/login.css') }}">
+@endsection
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+@section('additional-js')
+    <script src="{{ asset('View/js/login.js') }}"></script>
+@endsection
+
+@section('content')
+    <div class="main">
+
+        <div class="form">
+            <div class="text-message">
+                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
             </div>
-        </form>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+            @if (session('status') == 'verification-link-sent')
+                <div class="text-message">
+                    {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                </div>
+            @endif
 
-            <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+            <form method="POST" action="{{ route('verification.send') }}">
+                @csrf
+                <div class="text-center">
+                    <button type="submit" class="btn">
+                        {{ __('Resend Verification Email') }}
+                    </button>
+                </div>
+            </form>
+
+            <div class="mt-4 text-center">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn-secondary">
+                        {{ __('Log Out') }}
+                    </button>
+                </form>
+            </div>
+        </div>
+
+        <div class="link text-center">
+            <a class="register-link" href="{{ route('login') }}">Login</a>
+        </div>
     </div>
-</x-guest-layout>
+@endsection
